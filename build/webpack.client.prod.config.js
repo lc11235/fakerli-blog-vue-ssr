e4d9064710c4d889dca5514f867e14c398a7f648
lc11235 <lc11235@qq.com>
2017-07-19 00:09:20 +0800

    add backend api 20170719

---
+++ build/webpack.client.prod.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const SWPrecachePlugin = require('sw-precache-webpack-plugin');
+
+const srcDir = path.resolve(__dirname, '../dist').replace(/\\/g, "\/");
+const prefixMulti = {};
+prefixMulti[srcDir] = '';
+
+module.exports = {
+    devtool: false,
+    module: {
+        rules: [{
+            test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+            loader: 'url-loader',
+            query: {
+                limit: 10000,
+                name: 'static/img/[name].[hash:7].[ext]'
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader'])
+        }, {
+            test: /\.less$/,
+            loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader', 'less-loader'])
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('static/css/[name].[hash:7].css'),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks(module, count) {
+                return (module.resource && /\.js$/.test(module.resource) && module.resource.indexOf('node_modules') > 0)
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'manifest', chunks: ['vendor'] }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warning: false
+            },
+            output: {
+                comments: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new SWPrecachePlugin({
+            cacheId: 'fakerli-blog-vue-ssr',
+            filename: 'service-worker.js',
+            dontCacheBustUrlsMatching: /./,
+            staticFileGlobsIgnorePatterns: [/server\.html$/, /admin\.html$/, /\.map$/],
+            stripPrefixMulti: prefixMulti
+        }),
+        new HtmlWebpackPlugin({
+            chunks: [
+                'manifest', 'vendor', 'app'
+            ],
+            filename: 'server.html',
+            template: 'src/template/server.html',
+            inject: true,
+            chunksSortMode(chunk1, chunk2) {
+                let orders = ['manifest', 'vendor', 'app'];
+                let order1 = orders.indexOf(chunk1.names[0]);
+                let order2 = orders.indexOf(chunk2.names[0]);
+                return order1 - order2;
+            }
+        }),
+        new HtmlWebpackPlugin({
+            chunks: [
+                'manifest', 'vendor', 'admin'
+            ],
+            filename: 'admin.html',
+            template: 'src/template/admin.html',
+            inject: true,
+            chunksSortMode(chunk1, chunk2) {
+                let orders = ['manifest', 'vendor', 'admin'];
+                let order1 = orders.indexOf(chunk1.names[0]);
+                let order2 = orders.indexOf(chunk2.names[0]);
+                return order1 - order2;
+            }
+        })
+    ]
+};
---
+++ server/api/backend-article.js
@@ -0,0 +1,187 @@
+const moment = require('moment');
+const mongoose = require('../mongoose');
+const Article = mongoose.model('Article');
+const Category = mongoose.model('Category');
+const general = require('./general');
+
+const list = general.list;
+const item = general.item;
+
+const marked = require('marked');
+const hljs = require('highlight.js');
+marked.setOptions({
+    highlight(code) {
+        return hljs.highlightAuto(code).value;
+    },
+    breaks: true
+});
+
+/**
+ * 管理时，获取文章列表
+ * @method
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
+exports.getList = (req, res) => {
+    list(req, res, Article, '-update_date');
+};
+
+/**
+ * 管理时，获取单篇文章
+ * @method
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
+exports.getItem = (req, res) => {
+    itme(req, res, Article);
+};
+
+/**
+ * 发布文章
+ * @method
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
+exports.insert = (req, res) => {
+    let categorys = req.body.category,
+        content = req.body.content,
+        html = marked(content),
+        title = req.body.title;
+    let arr_category = categorys.split("|");
+    let category = arr_category[0];
+    let category_name = arr_category[1];
+    let data = {
+        title,
+        category,
+        category_name,
+        content,
+        html,
+        visit: 0,
+        like: 0,
+        comment_count: 0,
+        create_date: moment().format('YYYY-MM-DD HH:mm:ss'),
+        update_date: moment().format('YYYY-MM-DD HH:mm:ss'),
+        is_delete: 0,
+        timestamp: moment().format('X')
+    };
+    Article.createAsync(data).then(result => {
+        return Category.updateAsync({ _id: category }, { '$inc': { 'cate_num': 1 } }).then(() => {
+            return res.json({
+                code: 200,
+                message: '发布成功',
+                data: result
+            });
+        });
+    }).catch(err => {
+        res.json({
+            code: -200,
+            message: err.toString()
+        });
+    });
+};
+
+/**
+ * 管理时，删除文章
+ * @method
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
+exports.deletes = (req, res) => {
+    let id = req.query.id;
+    Article.updateAsync({ _id: id }, { is_delete: 1 }).then(() => {
+        return Category.updateAsync({ _id: id }, { '$inc': { 'cate_num': 1 } }).then(result => {
+            res.json({
+                code: 200,
+                message: '更新成功',
+                data: result
+            });
+        });
+    }).catch(err => {
+        res.json({
+            code: -200,
+            message: err.toString()
+        });
+    });
+};
+
+/**
+ * 管理时，恢复文章
+ * @method
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
+exports.recover = (req, res) => {
+    let id = req.query.id;
+    Article.updateAsync({ _id: id }, { is_delete: 0 }).then(() => {
+        return Category.updateAsync({ _id: id }, { '$inc': { 'cate_num': 1 } }).then(() => {
+            res.json({
+                code: 200,
+                message: '更新成功',
+                data: 'success'
+            });
+        });
+    }).catch(err => {
+        res.json(err => {
+            code: -200,
+                message: err.toString()
+        });
+    });
+};
+
+/**
+ * 管理时，编辑文章
+ * @method
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
+exports.modify = (req, res) => {
+    let category = req.body.category,
+        category_old = req.body.category_old,
+        content = req.body.content,
+        html = marked(content),
+        id = req.body.id;
+    let data = {
+        title: req.body.title,
+        category: req.body.category,
+        category_name: req.body.category_name,
+        content,
+        html,
+        update_date: moment().format('YYYY-MM-DD HH:mm:ss')
+    };
+    Article.findOneAndUpdateAsync({ _id: id }, data, { new: true }), then(result => {
+        if (category !== category_old) {
+            Promise.all([
+                Category.updateAsync({ _id: category }, { '$inc': { 'cate_num': 1 } }),
+                Category.updateAsync({ _id: category_old }, { '$inc': { 'cate_num': -1 } })
+            ]).then(() => {
+                res.json({
+                    code: 200,
+                    message: '更新成功',
+                    data: result
+                });
+            }).catch(err => {
+                res.json({
+                    code: -200,
+                    message: err.toString()
+                })
+            });
+        } else {
+            res.json({
+                code: 200,
+                message: '更新成功',
+                data: result
+            });
+        }
+    }).catch(err => {
+        res.json({
+            code: -200,
+            message: err.toString()
+        })
+    });
+};
