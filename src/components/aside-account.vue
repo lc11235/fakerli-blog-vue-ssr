67cc1df8438147bed5e838e2fed9677a4b766b20
lc11235 <lc11235@qq.com>
2017-07-24 00:50:00 +0800

    add components: account and input 20170724

---
+++ src/components/aside-account.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card card-me">
+        <router-link to="/user/account" active-class="active" class="side-entry">
+            <i class="icon icon-arrow-right"></i>
+            <i class="icon icon-articles"></i>
+            账号
+        </router-link>
+        <router-link to="/user/password" active-class="active" class="side-entry">
+            <i class="icon icon-arrow-right"></i>
+            <i class="icon icon-articles"></i>
+            密码
+        </router-link>
+        <a @click="handleLogout" href="javascript:;" class="side-entry">
+            <i class="icon icon-arrow-right"></i>
+            <i class="icon icon-arrticles"></i>
+            退出
+        </a>
+    </div>
+</template>
+
+<script lang="babel">
+    import api from '~api';
+    export default {
+        name: 'aside-account',
+        serverCachekey: () => {
+            return `aside::account`
+        },
+        methods: {
+            async handleLogout() {
+                await api.post('frontend/usertr/logout', {});
+                window.location.href = '/';
+            }
+        }
+    }
+</script>
