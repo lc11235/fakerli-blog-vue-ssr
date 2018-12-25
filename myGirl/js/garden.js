2739a09e13421860da1e51faf4103be5e8e8f438
lc11235 <lc11235@qq.com>
2018-12-25 13:21:37 +0800

    add audio support

--- myGirl/js/garden.js
+++ myGirl/js/garden.js
@@ -160,12 +160,12 @@
 	        density: 10,
 	        growSpeed: 1000 / 60,
 	        color: {
-				rmin: 128,
+				rmin: 0,
 				rmax: 255,
-				gmin: 0,
-				gmax: 128,
-				bmin: 0,
-				bmax: 128,
+				gmin: 128,
+				gmax: 255,
+				bmin: 128,
+				bmax: 255,
 	            opacity: 0.1
 	        },
 	        tanAngle: 60
