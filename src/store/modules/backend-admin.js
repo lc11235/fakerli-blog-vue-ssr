78d4b34fac1f90a8b0372f837f098a80f7a3de40
lc11235 <lc11235@qq.com>
2018-12-29 12:30:28 +0800

    add new

--- src/store/modules/backend-admin.js
+++ src/store/modules/backend-admin.js
@@ -103,6 +103,15 @@ const mutations = {
     },
     setMessageTrashList(state, list) {
         state.messageTrashList = list;
+    },
+    updateMessageContentStore(state, { msg_id, content }) {
+        state.messageContentStore[msg_id] = content;
+    },
+    moveMsg(state, { from, to, msg_id }) {
+        const index = state[from].findIndex(_ => _.msg_id === msg_id);
+        const msgItem = state[from].splice(index, 1)[0];
+        msgItem.loading = false;
+        state[to].unshift(msgItem);
     }
 };
 
@@ -112,7 +121,10 @@ const getters = {
     },
     'getAdminItem'(states) {
         return states.item;
-    }
+    },
+    messageUnreadCount: state => state.messageUnreadList.length,
+    messageReadedCount: state => state.messageReadedList.length,
+    messageTrashCount: state => state.messageTrashList.length
 };
 
 const actions = {
@@ -134,6 +146,85 @@ const actions = {
                 path
             });
         }
+    },
+    // 登录
+    handleLogin({ commit }, { userName, password }) {
+        userName = userName.trim();
+        return new Promise((resolve, reject) => {
+            login({
+                userName,
+                password
+            }).then(res => {
+                const data = res.data;
+                commit('setToken', data.token);
+                resolve();
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+    // 退出登录
+    handleLogOut({ state, commit }) {
+        return new Promise((resolve, reject) => {
+            logout(state.token).then(() => {
+                commit('setToken', '');
+                commit('setAccess', []);
+                resolve();
+            }).catch(err => {
+                reject(err);
+            });
+            // 如果你的退出登录无需请求接口，则可以直接使用下面三行代码而无需使用logout调用接口
+            // commit('setToken', '')
+            // commit('setAccess', [])
+            // resolve()
+        });
+    },
+    // 获取用户相关信息
+    getUserInfo({ state, commit }) {
+        return new Promise((resolve, reject) => {
+            try {
+                getUserInfo(state.token).then(res => {
+                    const data = res.data;
+                    commit('setAvator', data.avator);
+                    commit('setUserName', data.name);
+                    commit('setUserId', data.user_id);
+                    commit('setAccess', data.access);
+                    commit('setHasGetInfo', true);
+                    resolve(data);
+                }).catch(err => {
+                    reject(err);
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    },
+    // 此方法用来获取未读消息条数，接口只返回数值，不返回消息列表
+    getUnreadMessageCount({ state, commit }) {
+        getUnreadCount().then(res => {
+            const { data } = res;
+            commit('setMessageCount', data);
+        });
+    },
+    // 获取消息列表，其中包含未读、已读、回收站三个列表
+    getMessageList({ state, commit }) {
+        return new Promise((resolve, reject) =>{
+            getMessage().then(res => {
+                const { unread, readed, trash } = res.data;
+                commit('setMessageUnreadList', unread.sort((a, b) => new Date(b.create_time) - new Date(a.create_time)));
+                commit('setMessageReadedList', readed.map(_ => {
+                    _.loading = false;
+                    return _;
+                }).sort((a, b) => new Date(b.create_time) - new Date(a.create_time)));
+                commit('setMessageTrashList', trash.map(_ => {
+                    _.loading = false;
+                    return _;
+                }).sort((a, b) => new Date(b.create_time) - new Date(a.create_time)));
+                resolve();
+            }).catch(error => {
+                reject(error);
+            });
+        });
     }
 };
 
