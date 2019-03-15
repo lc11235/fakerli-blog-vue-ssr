0943b1aef8b78ea1fba5b1968487d6d9653a4d2f
changli <lc11235@users.noreply.github.com>
2019-03-15 17:30:03 +0800

    add new

--- server/api/yeting/love.js
+++ server/api/yeting/love.js
@@ -2,6 +2,11 @@ const moment = require('moment');
 const mongoose = require('mongoose');
 const Love = mongoose.model('Love');
 
+
+exports.getLoveList = (req, res) => {
+    
+};
+
 exports.insertNewThing = (req, res) => {
     let title = req.body.title;
     let content = req.body.content;
